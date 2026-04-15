55d7319bef133c7ef01c3ac1794f2502ef69ce23
MariaSHansen <maria-h-slavensky@hotmail.com>
2026-04-15 10:54:23 +0200

    DAG 2 - færdig med Opgave 2 (arrays og loops)

--- app.js
+++ app.js
@@ -4,13 +4,36 @@ console.log("Movie App starter...");
 
 // Man kan også samle sit array i ét array, i stedet for at lave én variabel per film//
 
-const movie = ["Inception", "The Matrix", "Intersteller", "The Dark Knight", "Pulp Fiction"];
+const movies = ["Inception", "The Matrix", "Intersteller", "The Dark Knight", "Pulp Fiction", "Barbie"];
 
-console.log("Alle film:", movie);
+console.log("Alle film:", movies);
 
 console.log("Første film:", movies[0]);
-console.log("Anden film:", movies[0]);
-console.log("Sidste film:", movies[0]);
+console.log("Anden film:", movies[1]);
+console.log("Sidste film:", movies[3]);
 
 console.log("Antal film:", movies.length);
+
+const movieList = document.querySelector("#movie-list");
+console.log(movieList);
+
+for (const movie of movies) {
+  const html = /* html */ `
+  <article class="movie-card">
+    <div class="movie-info">
+        <h3>${movie}</h3>
+    </div>
+  </article>
+  `;
+
+  movieList.insertAdjacentHTML("beforeend", html);
+}
+
+
+
+
+
+
+
+
 
