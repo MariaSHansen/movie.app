d6190765e035081293916a7db1fce70b1b958daa
MariaSHansen <maria-h-slavensky@hotmail.com>
2026-04-15 08:39:43 +0200

    dag 1
    Alt der er lavet dag 1

---
+++ app.js
@@ -0,0 +1,21 @@
+const countDisplay = document.querySelector("#counter");
+const clickButton = document.querySelector("#click-button");
+const resetButton = document.querySelector("#reset-button");
+
+console.log(countDisplay);
+console.log(clickButton);
+console.log(resetButton);
+
+let count = 0;
+
+clickButton.addEventListener("click", function () {
+  //count er en let variabel // 
+    count = count + 1;
+  countDisplay.textContent = count;
+});
+
+resetButton.addEventListener("click", function () {
+  count = 0;
+  countDisplay.textContent = count;
+});
+
